f44e1250950602ef1dde5c0d7c1d773517b6c808
xudean <xuda.it@outlook.com>
2023-11-06 17:27:01 +0800

    add batch create schema script

--- contracts/script/pado/getSchema.js
+++ contracts/script/pado/getSchema.js
@@ -7,48 +7,28 @@ async function main() {
     const commonSchemaStr = "string proofType2, string source, string content, string condition, bytes32 sourceUseridHash, bool result, uint64 timestamp, bytes32 useridHash";
     const assetProofSchemaStr = "string source,bytes32 sourceUseridHash,bytes32 authUseridHash,address receipt,uint64 getDataTime,uint64 baseValue,bool balanceGreaterThanBaseValue";
     const tokenHoldingSchemaStr = "string source,bytes32 sourceUseridHash,bytes32 authUseridHash,address recipient,uint64 getDataTime,string asset,string baseAmount,bool balanceGreaterThanBaseAmount";
-    const identityProofSchemaStr = "string source,string credentialType,bytes32 authUseridHash,address recipient,uint64 timestamp,bool result";
+    const identityProofSchemaStr  = "string source,string credentialType,bytes32 authUseridHash,address recipient,uint64 timestamp,bool result";
     const assetProofSchemaPolygonStr = "string source,bytes32 sourceUseridHash,bytes32 authUseridHash,string issuerDID,string recipientDID,address recipient,uint64 getDataTime,uint64 baseValue,bool balanceGreaterThanBaseValue";
     const tokenHoldingSchemaPolygonStr = "string source,bytes32 sourceUseridHash,bytes32 authUseridHash,string issuerDID,string recipientDID,address recipient,uint64 getDataTime,string asset,string baseAmount,bool balanceGreaterThanBaseAmount";
     const identityProofSchemaPolygonStr = "string source,string credentialType,bytes32 authUseridHash,string issuerDID,string recipientDID,address recipient,uint64 timestamp,bool result";
 
-    const schemaMap = {
-        assetProof: assetProofSchemaStr,
-        assetProofPolygon: assetProofSchemaPolygonStr,
-        tokenHolding: tokenHoldingSchemaStr,
-        tokenHoldingPolygon: tokenHoldingSchemaPolygonStr,
-        identityProof: identityProofSchemaStr,
-        identityProofPolygon: identityProofSchemaPolygonStr,
-    };
-
     console.log(
-        "Deploying contracts with the account:",
+        "Operation with the account:",
         deployer.address
     );
     //schema registry
     const contract = await hre.ethers.getContractAt("SchemaRegistry",
-        '0x7B9410e815C65f291dC69741A504DBA790797D43'
+        '0xB2c4Da1f8F08A0CA25862509E5431289BE2b598B'
     );
-    for (const key in schemaMap) {
-
-        try {
-            let shcemaId = await contract.getIdFromSchemaString(schemaMap[key]);
-            let schemaIdIsExist = await contract.isRegistered(shcemaId)
-            console.log(`${key} id is :${shcemaId}, exist: ${schemaIdIsExist}`)
-            if (!schemaIdIsExist) {
-                console.log("start to createSchema!")
-                //if not exists, start createSchema
-                let createSchemaTransactionResponse = await contract.createSchema(key, "PADO_SCHEMA", "https://padolabs.org", schemaMap[key]);
-                console.log(` ${key} please check schemaUID at tx Hash: ${createSchemaTransactionResponse.hash}`)
-            }
-
-        } catch (er) {
-            if (er.data && contract) {
-                const decodedError = contract.interface.parseError(er.data);
-                console.log(`Transaction failed: ${decodedError?.name}`);
-            } else {
-                console.log(`Error in widthrawContract:`, er);
-            }
+    try {
+        let schema = await contract.getSchema("0xF406CE092700219C3BE1729E52A0BC9539177E96B4A63D73D1E846CA20E1E900");
+        console.log(schema)
+    } catch (er) {
+        if (er.data && contract) {
+            const decodedError = contract.interface.parseError(er.data);
+            console.log(`Transaction failed: ${decodedError?.name}`);
+        } else {
+            console.log(`Error in request:`, er);
         }
     }
 
