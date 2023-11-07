df1abeeb2e6aa8ddd509a98c4aeaccb83bbee7eb
xudean <xuda.it@outlook.com>
2023-11-07 10:43:00 +0800

    add schemaUid for PADOPortal

--- contracts/script/pado/deployAndRegisterPadoPortal.js
+++ contracts/script/pado/deployAndRegisterPadoPortal.js
@@ -9,12 +9,12 @@ async function main() {
 
     console.log("Deploying PADOPortalUpgradeable...");
     const fee = "300000000000000";
-    const receiveAddr = "0x9717BdADb90a18e040e835b665f9E51eAa101ab1";
-    const RouterAddress = "0x736c78b2f2cBf4F921E8551b2acB6A5Edc9177D5"; // linea testnet
+    const receiveAddr = "0x024e45D7F868C41F3723B13fD7Ae03AA5A181362";
+    const RouterAddress = "0xBc64cDf29e598F7a6917ab23bAB24A815891bE71"; // linea testnet
     //const RouterAddress = "0x4d3a380A03f3a18A5dC44b01119839D8674a552E"; // linea mainnet
     const padoportalcontract = await hre.ethers.getContractFactory("PADOPortalUpgradeable");
     const padoportal = await hre.upgrades.deployProxy(padoportalcontract,
-        ["PADO Portal", fee, receiveAddr, [], RouterAddress], {initializer: 'initialize'});
+        ["PADO Portal", fee, receiveAddr, [], RouterAddress,"0xea3441099f6764cd494e7076d797b439aaf88f0693608a0557e389dfebcff8e9"], {initializer: 'initialize'});
     await padoportal.waitForDeployment();
     const padoportalProxyAddress = await padoportal.getAddress();
     const padoportalImplementationAddress = await hre.upgrades.erc1967.getImplementationAddress(padoportalProxyAddress);
@@ -26,7 +26,7 @@ async function main() {
     console.log(`Implementation is at ${padoportalImplementationAddress}`);
     console.log(`adminAddress is at ${adminAddress}`);
 
-    const PortalRegistry = "0x506f88a5Ca8D5F001f2909b029738A40042e42a6"; // linea testnet
+    const PortalRegistry = "0x73d3654Ed0A308F646886516A53883dCB28DBaCB"; // linea testnet
     //const PortalRegistry = "0xd5d61e4ECDf6d46A63BfdC262af92544DFc19083"; // linea mainnet
     const contract = await hre.ethers.getContractAt("PortalRegistry", PortalRegistry);
     try {
