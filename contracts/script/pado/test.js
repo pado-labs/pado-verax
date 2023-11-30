05851432bc92f0f3f0d3ed85ce39dd4ee829fd8b
fksyuan <fksyuan@163.com>
2023-11-30 11:01:36 +0800

    update test script

--- contracts/script/pado/test.js
+++ contracts/script/pado/test.js
@@ -9,7 +9,8 @@ async function main() {
 
     //const padoContractAddress = "0x8A047d2FbcEC425D7A28de7bce3630f38eC497c6"; // scroll sepolia testnet
     //const padoContractAddress = "0x1A58B9E0Aae7990466eA70d6791876EDcab67Ea7"; // scroll mainnet
-    const padoContractAddress = "0x515b9dd670176e68b9623926d12082d6f37786f4"; // linea goerli testnet
+    //const padoContractAddress = "0x515b9dd670176e68b9623926d12082d6f37786f4"; // linea goerli testnet
+    const padoContractAddress = "0xc4B7dCba12866f6f8181b949ca443232C4e94334"; // linea mainnet
     const padoContract = await hre.ethers.getContractAt("PADOPortalUpgradeable", padoContractAddress);
     const userAddress = "0xDB736B13E2f522dBE18B2015d0291E4b193D8eF6";
     const schemaId = "0x84FDF5748D9AF166503472FF5DEB0CD5F61F006169424805FD5554356AC6DF10";
@@ -18,6 +19,14 @@ async function main() {
 
     let res = await padoContract.checkDeFiVoyageHumanityWithTwitter(userAddress);
     console.log("res=", res);
+    let res1 = await padoContract.checkDeFiVoyageHumanity(userAddress);
+    console.log("res1=", res1);
+    let res2 = await padoContract.fee();
+    console.log("res2=", res2);
+    let res3 = await padoContract.receiveAddr();
+    console.log("res3=", res3);
+    let resschemaid = await padoContract.getWebSchemaId();
+    console.log("resschemaid=", resschemaid);
 
     //let resSetModuleRegistry = await padoContract.setModuleRegistry("0x50bd377EB8D4236Bb587AB3FB1eeafd888AEeC58");
     //console.log("resSetModuleRegistry=", resSetModuleRegistry);
@@ -36,10 +45,10 @@ async function main() {
     // console.log("resCheck2=", resCheck2);
 
     //const AttestationRegistry = "0xC765F28096F6121C2F2b82D35A4346280164428b"; // linea testnet
-    //const AttestationRegistry = ""; // linea mainnet
-    /*const AttestationRegistry = "0xC769999Dd0abBB7007F6F9aF58aA17F4C45aa3bA"; // scroll sepolia testnet
+    const AttestationRegistry = "0x3de3893aa4Cdea029e84e75223a152FD08315138"; // linea mainnet
+    //const AttestationRegistry = "0xC769999Dd0abBB7007F6F9aF58aA17F4C45aa3bA"; // scroll sepolia testnet
     const contract = await hre.ethers.getContractAt("AttestationRegistry", AttestationRegistry);
-    const astId = "0x0000000000000000000000000000000000000000000000000000000000000001";
+    const astId = "0x000000000000000000000000000000000000000000000000000000000003e573";
     const ats = await contract.getAttestation(astId);
     console.log("ats=", ats);
     const decodedata = hre.ethers.AbiCoder.defaultAbiCoder().decode(
@@ -47,7 +56,7 @@ async function main() {
         ats[11]
     );
     console.log("decodedata=", decodedata);
-    const astId1 = "0x0000000000000000000000000000000000000000000000000000000000000002";
+    /*const astId1 = "0x0000000000000000000000000000000000000000000000000000000000000002";
     const ats1 = await contract.getAttestation(astId1);
     console.log("ats1=", ats1);
     const decodedata1 = hre.ethers.AbiCoder.defaultAbiCoder().decode(
